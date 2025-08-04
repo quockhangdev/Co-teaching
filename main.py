e77cedabe0403830f5cf3b9cc634f0d63689fbe4
Quoc-Khang Tran <quockhang@192.168.2.7>
2025-08-04 16:19:02 +0700

    huhu

--- main.py
+++ main.py
@@ -47,16 +47,30 @@ input_channel, num_classes = 3, 12  # default for CXR
 
 if args.dataset == "cxr":
     train_dataset = CXR(
-        root="/Users/quockhang/Documents/dev/Data-Yte-AG/img_xray",
+        root="/home/khmt/Documents/MyKhanh/GNN/data/img_xray",
         train=True,
-        transform=transforms.ToTensor(),
+        transform=transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(10),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            ]
+        ),
         noise_type=args.noise_type,
         noise_rate=args.noise_rate,
     )
     test_dataset = CXR(
-        root="/Users/quockhang/Documents/dev/Data-Yte-AG/img_xray",
+        root="/home/khmt/Documents/MyKhanh/GNN/data/img_xray",
         train=False,
-        transform=transforms.ToTensor(),
+        transform=transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+            ]
+        ),
         noise_type=args.noise_type,
         noise_rate=args.noise_rate,
     )
