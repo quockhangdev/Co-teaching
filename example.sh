762cccd3d2791dc62b3adfc648dc15d1e648f0c1
Quoc-Khang Tran <quockhang@192.168.2.7>
2025-08-04 22:53:00 +0700

    noise

--- example.sh
+++ example.sh
@@ -10,13 +10,15 @@ export CUDA_VISIBLE_DEVICES=0
 
 # Define arguments
 DATASET="cxr"
-NOISE_TYPE="symmetric"
+# NOISE_TYPE="symmetric"
+NOISE_TYPE="pairflip"
 NOISE_RATE=0.2
 BATCH_SIZE=16
-LR=1e-3
+LR=1e-4
 EPOCHS=200
 WORKERS=16
 RESULT_DIR="results"
+NUM_ITER_PER_EPOCH=1000
 
 # Run the training script with AMP
 python main.py \
@@ -26,4 +28,5 @@ python main.py \
     --lr $LR \
     --n_epoch $EPOCHS \
     --num_workers $WORKERS \
-    --result_dir $RESULT_DIR
+    --result_dir $RESULT_DIR \
+    --num_iter_per_epoch $NUM_ITER_PER_EPOCH
